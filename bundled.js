ddfb2b74452dc02729ba7cc6723ef60d330fe929
sudodoki <smd.deluzion@gmail.com>
2015-07-04 10:31:25 +0200

    Update build

--- bundled.js
+++ bundled.js
@@ -1,32 +1,43 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function copy(text) {
-  var range = document.createRange();
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode(text);
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv);
-  range.selectNode(newDiv);
-  window.getSelection().addRange(range);
   try {
+    var range = document.createRange();
+    var selection = document.getSelection();
+
+    var mark = document.createElement('mark');
+    mark.textContent = text;
+    document.body.appendChild(mark);
+
+    range.selectNode(mark);
+    selection.addRange(range);
+
     var successful = document.execCommand('copy');
     if (!successful) {
-      throw(new Error('copy command was unsuccessful'));
+      throw new Error('copy command was unsuccessful');
     }
-  } catch(err) {
+  } catch (err) {
     console.error('unable to copy, trying IE specific stuff');
     try {
-      window.clipboardData.setData("Text", text);
-    } catch(err) {
-      console.error('unable to copy, fallback to prompt');
-      window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+      window.clipboardData.setData('text', text);
+    } catch (err) {
+      console.error('unable to copy, falling back to prompt');
+      window.prompt('Copy to clipboard: Ctrl+C, Enter', text);
     }
   } finally {
-    // Remove the selections - NOTE: Should use
-    // removeRange(range) when it is supported
-    document.body.removeChild(newDiv);
-    window.getSelection().removeAllRanges();
+    if (selection) {
+      if (typeof selection.removeRange == 'function') {
+        selection.removeRange(range);
+      } else {
+        selection.removeAllRanges();
+      }
+    }
+
+    if (mark) {
+      document.body.removeChild(mark);
+    }
   }
 }
+
 module.exports = copy;
 },{}],2:[function(require,module,exports){
 /*! (C) WebReflection Mit Style License */
